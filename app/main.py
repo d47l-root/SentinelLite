4b6efc1a835c5695f9d71b265d6ae65c55130d2d
Nadim mojadadi <nadeemdego@gmail.com>
2026-08-06 13:40:26 +0300

    chore: add environment variable configuration

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from app.core.config import settings
 
 app = FastAPI()
 
 @app.get("/health")
 def health_check():
-    return {"status":"alive"}
+    return {"status":"alive","app_name":settings.app_name,"debug": settings.debug}
